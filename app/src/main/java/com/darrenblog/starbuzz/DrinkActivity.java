2f1beb19d85fa487252647f3d1aa6b3ca3b6225d
Darren1998 <hnsjyslzp@163.com>
2019-02-15 21:51:37 +0800

    Add "Favorite" Checkbox for drink.

--- app/src/main/java/com/darrenblog/starbuzz/DrinkActivity.java
+++ app/src/main/java/com/darrenblog/starbuzz/DrinkActivity.java
@@ -1,11 +1,14 @@
 package com.darrenblog.starbuzz;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,12 +28,13 @@ public class DrinkActivity extends AppCompatActivity {
         SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
         try {
             SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
-            Cursor cursor = db.query("DRINK",new String[]{"NAME","DESCRIPTION","IMAGE_RESOURCE_ID"},
+            Cursor cursor = db.query("DRINK",new String[]{"NAME","DESCRIPTION","IMAGE_RESOURCE_ID","FAVORITE"},
                     "_id=?", new String[]{Integer.toString(drinkId)},null,null,null);
             if (cursor.moveToFirst()){
                 String nameText = cursor.getString(0);
                 String descriptionText = cursor.getString(1);
                 int photoId = cursor.getInt(2);
+                boolean isFavorite = (cursor.getInt(3) == 1);
 
                 TextView name = findViewById(R.id.name);
                 name.setText(nameText);
@@ -41,8 +45,28 @@ public class DrinkActivity extends AppCompatActivity {
                 ImageView photo = findViewById(R.id.photo);
                 photo.setImageResource(photoId);
                 photo.setContentDescription(nameText);
+
+                CheckBox favorite = findViewById(R.id.favorite);
+                favorite.setChecked(isFavorite);
             }
             cursor.close();
+            db.close();
+        } catch (SQLException e) {
+            Toast.makeText(this,"Database unavailable",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onFavoriteClicked(View view) {
+        int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
+
+        CheckBox favorite = findViewById(R.id.favorite);
+        ContentValues drinkvalues = new ContentValues();
+        drinkvalues.put("FAVORITE",favorite.isChecked());
+
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            SQLiteDatabase db = starbuzzDatabaseHelper.getWritableDatabase();
+            db.update("DRINK", drinkvalues, "_id = ?", new String[]{Integer.toString(drinkId)});
             db.close();
         } catch (SQLException e) {
             Toast.makeText(this,"Database unavailable",Toast.LENGTH_SHORT).show();
